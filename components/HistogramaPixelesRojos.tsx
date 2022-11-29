0753da1f30dbf5efdb2770bc3253a52539ad9970
Samuel Guerrero <itougn15@gmail.com>
2022-11-29 14:05:15 -0600

    Added Histograma Pixeles Rojos

--- components/HistogramaPixelesRojos.tsx
+++ components/HistogramaPixelesRojos.tsx
@@ -1,9 +1,15 @@
 import { Dispatch, SetStateAction, useState } from "react";
 
+import { Button } from "./Button";
 import { ImageCard } from "./ImageCard";
 import { Input } from "./Input";
+import { start } from "repl";
 
-const HistogramaAlgoritm = (setChannel: Dispatch<SetStateAction<any[]>>, redPixel: number) => {
+const HistogramaAlgoritm = (setChannel: Dispatch<SetStateAction<any[]>>,
+    setPixelsRange: Dispatch<SetStateAction<{
+        start: number;
+        end: number;
+    }>>, initialPoint: number, endPoint: number) => {
     var canvas1 = document.getElementById(
         "canvasImagen1"
     ) as HTMLCanvasElement | null;
@@ -29,6 +35,9 @@ const HistogramaAlgoritm = (setChannel: Dispatch<SetStateAction<any[]>>, redPixe
     function histograma() {
         var channelValues = new Array(256).fill(0)
 
+        console.log(initialPoint, endPoint)
+
+
         var image = new Image() as any;
         image = ctx1.getImageData(0, 0, canvas1.width, canvas1.height);
         const pixels = image.data;
@@ -41,7 +50,7 @@ const HistogramaAlgoritm = (setChannel: Dispatch<SetStateAction<any[]>>, redPixe
 
 
         for (var i = 0; i < numPixels; i++) {
-            if (pixels[i * 4] == redPixel)
+            if ((pixels[i * 4] >= initialPoint) && (pixels[i * 4] <= endPoint))
                 changePixels.push(i)
             channelValues[pixels[i * 4]] += 1;
         }
@@ -59,39 +68,49 @@ const HistogramaAlgoritm = (setChannel: Dispatch<SetStateAction<any[]>>, redPixe
             pixels[changePixels[i] * 4 + 1] = 0
             pixels[changePixels[i] * 4 + 2] = 0
         }
-
-
         ctx2.putImageData(image, 0, 0);
     }
 };
 
 
-export const HistogramaModificado = () => {
+export const HistogramaPixelesRojos = () => {
     const [channel, setChannel] = useState<any[]>([0])
-
+    const [pixelsRange, setPixelsRange] = useState({ start: -1, end: -1 })
 
     return (
         <div>
-            <Input idInput="imagen1" selectTool={() => HistogramaAlgoritm(setChannel, 0)} />
+            <Input idInput="imagen1" selectTool={() => HistogramaAlgoritm(setChannel, setPixelsRange, 0, 0)} />
 
             <div className="container mx-auto flex space-x-5 justify-center">
-                <ImageCard variantName={"Imagen Original"} target={0} />
-                <ImageCard variantName={"Imagen Modificada"} target={1} />
+                <div className="hidden">
+                    <ImageCard variantName={""} target={0} />
+                </div>
+                <ImageCard variantName={"Imagen"} target={1} />
             </div>
 
             {
                 channel.length > 1 && (
-                    <div className="w-full flex flex-col space-y-2 pb-5">
-                        <div className="flex flex-col bg-[#fffaeb] shadow-red-600 shadow-2xl w-min mx-auto ">
-                            <h1 className="text-center font-ramptartOne text-red-600 text-4xl">Red</h1>
+                    <div className="w-full flex flex-col items-center space-y-2 pb-5">
+                        <div className="flex flex-col bg-[#fffaeb] shadow-black shadow-2xl w-min mx-auto ">
+                            <h1 className="text-center font-ramptartOne text-4xl">Histograma</h1>
                             <div className="flex justify-center items-baseline">
                                 {channel.map((el, i) => (
-                                    <button onClick={() => HistogramaAlgoritm(setChannel, i)}
-                                        style={{ height: (el * 0.8) }}
-                                        className="w-[5.5px] border border-black hover:cursor-pointer bottom-0 bg-red-600" key={i} />
+                                    <button onClick={() => {
+                                        if (pixelsRange.start < 0) {
+                                            setPixelsRange({ ...pixelsRange, start: i })
+                                            return
+                                        }
+                                        if (pixelsRange.start > 0)
+                                            setPixelsRange({ ...pixelsRange, end: i })
+
+                                        HistogramaAlgoritm(setChannel, setPixelsRange, pixelsRange.start, i)
+                                    }}
+                                        style={{ height: (el * 0.5) }}
+                                        className={`w-[5px] border border-black hover:cursor-pointer bottom-0 ${i == pixelsRange.start ? 'bg-red-500' : pixelsRange.end == i ? 'bg-red-500' : 'bg-yellow-400'}`} key={i} />
                                 ))}
                             </div>
                         </div>
+                        <button className="bg-yellow-400 text-black font-bold py-2 px-4 rounded h-11 w-36 cursor-pointer" onClick={() => { setPixelsRange({ start: -1, end: -1 }); HistogramaAlgoritm(setChannel, setPixelsRange, 0, 0) }}>Reiniciar</button>
                     </div>
                 )
             }
